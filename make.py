a174f827d0091015ca054611607ff8863da4def4
田足井　昇太 <tataraishouta@taashiishoutanoMacBook-Air.local>
2021-07-08 10:45:58 +0900

    make

--- make.py
+++ make.py
@@ -29,7 +29,7 @@ dic={i:a for i,a in zip(d,x) }
 for i in range(len(X)):
     if dic[y[i]]<max_num:
         file_path="./data/img_test/img_"+y[i]+"_"+str(dic[y[i]])+".jpg"
-        im_f=(X[i].reshape(28, 28))  # 画像を28×28の形に変形
+        im_f=(X[str(i)].reshape(28, 28))  # 画像を28×28の形に変形
         pil_img_f = Image.fromarray(im_f.astype(np.uint8))  # 画像をPILに
         pil_img_f = pil_img_f.resize((64, 64), Image.BICUBIC)  # 64×64に拡大
         pil_img_f.save(file_path)  # 保存
