f28313fbc4394f2dfd0730b7e67f3a689c32403a
田足井　昇太 <tataraishouta@taashiishoutanoMacBook-Air.local>
2021-07-08 22:16:15 +0900

    make

--- make.py
+++ make.py
@@ -13,6 +13,9 @@ data_dir = "./data/"
 if not os.path.exists(data_dir):
     os.mkdir(data_dir)
 
+data_dir_path = "./data/img_test/"
+if not os.path.exists(data_dir_path):
+    os.mkdir(data_dir_path)
 #mnist = fetch_openml('mnist_784', version=1, data_home="./data/")
 mnist = fetch_openml('mnist_784', version=1, data_home="./data/")
 # data_homeは保存先を指定します
@@ -25,11 +28,11 @@ y = mnist.target
 max_num=200
 d=[str(i) for i in range(10)]
 x=np.zeros(10)
-dic={i:a for i,a in zip(d,x) }
+dic={i:int(a) for i,a in zip(d,x) }
 for i in range(len(X)):
     if dic[y[i]]<max_num:
         file_path="./data/img_test/img_"+y[i]+"_"+str(dic[y[i]])+".jpg"
-        im_f=(X[str(i)].reshape(28, 28))  # 画像を28×28の形に変形
+        im_f=(X[i].reshape(28, 28))  # 画像を28×28の形に変形
         pil_img_f = Image.fromarray(im_f.astype(np.uint8))  # 画像をPILに
         pil_img_f = pil_img_f.resize((64, 64), Image.BICUBIC)  # 64×64に拡大
         pil_img_f.save(file_path)  # 保存
