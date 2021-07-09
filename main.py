558c53e9e86bd3ceab10f04ae8a1c72eee58f937
田足井　昇太 <tataraishouta@taashiishoutanoMacBook-Air.local>
2021-07-09 16:40:43 +0900

    to numpy

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import numpy as np
 """
 beta1=0.0
 beta2=0.9
-optimizer_g=Adam(lr=0.000001,beta1=beta1,beta2=beta2)
+optimizer_g=Adam(lr=0.00000001,beta1=beta1,beta2=beta2)
 optimizer_d=Adam(lr=0.00004,beta1=beta1,beta2=beta2)
 batch_size=64
 
