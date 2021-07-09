b2bdddc72cf625769fa007891dca48847119efc8
田足井　昇太 <tataraishouta@taashiishoutanoMacBook-Air.local>
2021-07-09 15:02:13 +0900

    to numpy

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ import numpy as np
 """
 beta1=0.0
 beta2=0.9
-optimizer_g=Adam(lr=0.0001,beta1=beta1,beta2=beta2)
-optimizer_d=Adam(lr=0.0004,beta1=beta1,beta2=beta2)
+optimizer_g=Adam(lr=0.00001,beta1=beta1,beta2=beta2)
+optimizer_d=Adam(lr=0.00004,beta1=beta1,beta2=beta2)
 batch_size=64
 
 criterion_d=SoftmaxWithLoss()
