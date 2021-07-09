ffe4c17bc9033750c7a2ffb36bae4b93d80f9b64
田足井　昇太 <tataraishouta@taashiishoutanoMacBook-Air.local>
2021-07-09 15:32:36 +0900

    to numpy

--- model.py
+++ model.py
@@ -14,11 +14,11 @@ class Generater:
 
     def __init__(self,input_size=24,output_size=1,weight_init_std = 0.01,train=False):
         self.params = {}
-        self.params['W1'] = np.random.randn(128,20,4,4)
-        self.params['W2'] = np.random.randn(64,128,4,4)
-        self.params['W3'] = np.random.randn(32,64,4,4)
-        self.params['W4'] = np.random.randn(16,32,4,4)
-        self.params['W5'] = np.random.randn(1,16,3,3)
+        self.params['W1'] = np.random.randn(64,20,4,4)
+        self.params['W2'] = np.random.randn(32,64,4,4)
+        self.params['W3'] = np.random.randn(16,32,4,4)
+        self.params['W4'] = np.random.randn(8,16,4,4)
+        self.params['W5'] = np.random.randn(1,8,3,3)
 
         path='learned/save.pkl'
         if train:
